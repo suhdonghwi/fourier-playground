af066d275bd9f1dbd0758982501753b4d5fdedc2
Suh Donghwi <hwidongsuh@gmail.com>
2020-11-29 12:59:16 +0900

    feat: add vertex graph drawing

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -9,6 +9,9 @@ export default function Canvas() {
   const thetaDelta = 0.05;
   const trail: Point[] = [];
 
+  const circlePoint = {x: 200, y: 300};
+  const circleRadius = 100;
+
   const setup = (p5: p5Types, canvasParentRef: Element) => {
     // use parent to render the canvas in this ref
     // (without that p5 will render the canvas outside of your component)
@@ -17,25 +20,31 @@ export default function Canvas() {
 
   const draw = (p5: p5Types) => {
     p5.background(240);
-    p5.ellipse(200, 300, 200, 200);
+
+    p5.stroke(0, 0, 0);
+    p5.noFill();
+    p5.ellipse(circlePoint.x, circlePoint.y, circleRadius * 2, circleRadius * 2);
 
     const point = {
-      x: 200 + 100 * Math.cos(theta),
-      y: 300 - 100 * Math.sin(theta),
+      x: circlePoint.x + circleRadius * Math.cos(theta),
+      y: circlePoint.y - circleRadius * Math.sin(theta),
     };
     p5.ellipse(point.x, point.y, 10, 10);
 
     const trailPoint = {
-        x: 300,
+        x: circlePoint.x + circleRadius,
         y: point.y
     };
     p5.line(point.x, point.y, trailPoint.x, trailPoint.y);
     trail.push(trailPoint);
 
+    p5.beginShape();
     for (const point of trail) {
-        p5.ellipse(point.x, point.y, 5, 5);
+        p5.curveVertex(point.x, point.y);
         point.x += 2;
     }
+    p5.endShape();
+
     theta += thetaDelta;
   };
 
