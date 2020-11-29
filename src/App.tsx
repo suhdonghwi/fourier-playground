952e505b0aee8f840746dd3ac53569a1c64d06db
Suh Donghwi <hwidongsuh@gmail.com>
2020-11-29 18:18:58 +0900

    feat: add square example

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,23 @@ import fourierCoefficient from "./fourier/coefficient";
 
 function App() {
   function f(x: number) {
-    if (x <= 0.5) return math.complex(100 * x, 0);
-    else return math.complex(100 * x, 100);
+    if (x <= 0.25) {
+      return math.complex(300 * x, 0);
+    } else if (x <= 0.5) {
+      return math.complex(75, 300 * (x - 0.25));
+    } else if (x <= 0.75) {
+      return math.complex(75 - 300 * (x - 0.5), 75);
+    } else {
+      return math.complex(0, 75 - 300 * (x - 0.75));
+    }
+    /*if (x <= 0.5) return math.complex(100 * x, 0);
+    else return math.complex(100 * x, 100);*/
   }
 
-  const coeffs = fourierCoefficient(f, 50);
+  const coeffs = fourierCoefficient(f, 5);
   const circles: UnitCircle[] = [];
 
-  let n = -50;
+  let n = -5;
   for (const coeff of coeffs) {
     const polar = coeff.toPolar();
 
