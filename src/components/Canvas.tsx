38eee1a9c33679e91a99037e6fa54ec6e8e7dedd
Suh Donghwi <hwidongsuh@gmail.com>
2020-11-29 12:47:21 +0900

    feat: add trail graph drawing

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -2,22 +2,42 @@ import React from "react";
 import Sketch from "react-p5";
 import p5Types from "p5";
 
+import Point from "../types/Point";
+
 export default function Canvas() {
-    let theta = 0;
-    const thetaDelta = 0.05;
- 
-    const setup = (p5: p5Types, canvasParentRef: Element) => {
-        // use parent to render the canvas in this ref
-        // (without that p5 will render the canvas outside of your component)
-        p5.createCanvas(p5.windowWidth, 600).parent(canvasParentRef);
+  let theta = 0;
+  const thetaDelta = 0.05;
+  const trail: Point[] = [];
+
+  const setup = (p5: p5Types, canvasParentRef: Element) => {
+    // use parent to render the canvas in this ref
+    // (without that p5 will render the canvas outside of your component)
+    p5.createCanvas(p5.windowWidth, 600).parent(canvasParentRef);
+  };
+
+  const draw = (p5: p5Types) => {
+    p5.background(240);
+    p5.ellipse(200, 300, 200, 200);
+
+    const point = {
+      x: 200 + 100 * Math.cos(theta),
+      y: 300 - 100 * Math.sin(theta),
     };
- 
-    const draw = (p5: p5Types) => {
-        p5.background(240);
-        p5.ellipse(200, 300, 200, 200);
-        p5.ellipse(200 + 100 * Math.cos(theta), 300 - 100 * Math.sin(theta), 10, 10);
-        theta += thetaDelta;
+    p5.ellipse(point.x, point.y, 10, 10);
+
+    const trailPoint = {
+        x: 300,
+        y: point.y
     };
- 
-    return <Sketch setup={setup} draw={draw} />;
-};
+    p5.line(point.x, point.y, trailPoint.x, trailPoint.y);
+    trail.push(trailPoint);
+
+    for (const point of trail) {
+        p5.ellipse(point.x, point.y, 5, 5);
+        point.x += 2;
+    }
+    theta += thetaDelta;
+  };
+
+  return <Sketch setup={setup} draw={draw} />;
+}
