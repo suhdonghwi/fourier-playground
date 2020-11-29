da377ea9cc96c5bb5316b81bac2dc28d7c5f7855
Suh Donghwi <hwidongsuh@gmail.com>
2020-11-29 13:58:30 +0900

    feat: add graph mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,16 @@ import Canvas from "./components/Canvas";
 import UnitCircle from "./types/UnitCircle";
 
 function App() {
-  const circles: UnitCircle[] = [{ radius: 100, coefficient: 1 }, { radius: 50, coefficient: 100 }];
+  const circles: UnitCircle[] = [
+    { radius: 100, coefficient: 1 },
+    { radius: 10, coefficient: 0.1 },
+    { radius: 10, coefficient: 20 },
+    { radius: 30, coefficient: 30 },
+  ];
 
   return (
     <div className="App">
-      <Canvas unitCircles={circles} />
+      <Canvas unitCircles={circles} isGraphMode={true} />
     </div>
   );
 }
--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -7,14 +7,13 @@ import UnitCircle from "../types/UnitCircle";
 
 interface CanvasProps {
   unitCircles: UnitCircle[];
+  isGraphMode: boolean;
 }
 
-export default function Canvas({ unitCircles }: CanvasProps) {
+export default function Canvas({ unitCircles, isGraphMode }: CanvasProps) {
   let theta = 0;
   const thetaDelta = 0.05;
   const trail: Point[] = [];
-
-  const startPoint = { x: 200, y: 300 };
 
   const setup = (p5: p5Types, canvasParentRef: Element) => {
     // use parent to render the canvas in this ref
@@ -29,6 +28,9 @@ export default function Canvas({ unitCircles }: CanvasProps) {
     p5.noFill();
     p5.stroke(200);
 
+    const startPoint = isGraphMode
+      ? { x: 200, y: 300 }
+      : { x: p5.windowWidth / 2, y: 300 };
     let prevPoint = startPoint;
     for (const circle of unitCircles) {
       p5.ellipse(
@@ -44,26 +46,31 @@ export default function Canvas({ unitCircles }: CanvasProps) {
     }
 
     const point = prevPoint;
-    const trailPoint = {
-      x:
-        startPoint.x +
-        unitCircles.map((c) => c.radius).reduce((v1, v2) => v1 + v2),
-      y: point.y,
-    };
-
-    p5.noFill();
-    p5.stroke(200);
-    p5.line(point.x, point.y, trailPoint.x, trailPoint.y);
 
     p5.fill(0);
     p5.noStroke();
     p5.ellipse(point.x, point.y, 7, 7);
 
-    p5.fill(0);
-    p5.noStroke();
-    p5.ellipse(trailPoint.x, trailPoint.y, 7, 7);
+    if (isGraphMode) {
+      const trailPoint = {
+        x:
+          startPoint.x +
+          unitCircles.map((c) => c.radius).reduce((v1, v2) => v1 + v2),
+        y: point.y,
+      };
 
-    trail.push(trailPoint);
+      p5.noFill();
+      p5.stroke(200);
+      p5.line(point.x, point.y, trailPoint.x, trailPoint.y);
+
+      p5.fill(0);
+      p5.noStroke();
+      p5.ellipse(trailPoint.x, trailPoint.y, 7, 7);
+
+      trail.push(trailPoint);
+    } else {
+      trail.push(point);
+    }
 
     p5.beginShape();
     p5.stroke(255, 0, 0);
@@ -71,7 +78,8 @@ export default function Canvas({ unitCircles }: CanvasProps) {
     for (const point of trail) {
       if (point.x <= p5.windowWidth) {
         p5.curveVertex(point.x, point.y);
-        point.x += 2;
+
+        if (isGraphMode) point.x += 2;
       }
     }
     p5.endShape();
